0b31988eed95db8b9bd5b4b392107aa7ffcb7564
Matt Robinson <mrobinson7627@gmail.com>
2015-07-16 07:32:17 -0400

    Fixed some path issues

--- Main.hs
+++ Main.hs
@@ -6,21 +6,22 @@ import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 import Data.Yaml
+import System.Directory
 import System.Process
+import System.Environment
 import System.Exit
-import TextShow
-import Data.Monoid ((<>))
+import System.FilePath
 
 data RenderGroup = RenderGroup {
     backend    :: Backend,
-    images     :: Vector Text,
+    images     :: Vector FilePath,
     renderJobs :: Vector RenderJob
 } deriving (Show)
 
 instance FromJSON RenderGroup where
     parseJSON (Object v) = RenderGroup <$>
         v .: "backend" <*>
-        v .: "images" <*>
+        fmap (fmap T.unpack) (v .: "images") <*>
         v .: "render"
 
 data Backend = Illustrator | Inkscape
@@ -32,22 +33,22 @@ instance FromJSON Backend where
     parseJSON _                      = fail "Backend must be one of: illustrator, inkscape"
 
 data RenderJob = RenderJob {
-    jobPath  :: Text,
+    jobPath  :: FilePath,
     dpi      :: Maybe Float,
     srcDPI   :: Maybe Float,
     scaling  :: Maybe Float,
-    prepend  :: Maybe Text
+    prepend  :: Maybe String
 } deriving (Show)
 
 instance FromJSON RenderJob where
     parseJSON (Object v) = RenderJob <$>
-        v .:  "path" <*>
+        fmap T.unpack (v .:  "path") <*>
         v .:? "dpi" <*>
         v .:? "srcdpi" <*>
         v .:? "prepend" <*>
-        v .:? "scaling"
+        fmap (fmap T.unpack) (v .:? "scaling")
 
-illustrator :: Text -> RenderJob -> IO ()
+illustrator :: FilePath -> RenderJob -> IO ()
 illustrator input job =
     let srcdpi = fromMaybe 72 $ srcDPI job
         mScale = scaling job `mplus` (dpi job >>= \dp -> Just $ (dp / srcdpi) * 100)
@@ -55,33 +56,37 @@ illustrator input job =
         Nothing -> print "What are you doing"
         Just scale -> do
             let cmd = "osascript"
-                output = jobPath job <> input
+                output = jobPath job </> (replaceExtension input ".png")
                 args = [ "illustrator-render"
-                       , T.unpack input
-                       , T.unpack output
+                       , input
+                       , output
                        , show scale
                        ]
-            print $ "Rendering " <> input <> " to " <> output
+            print $ "Rendering " ++ input ++ " to " ++ output
             (ecode, out, err) <- readProcessWithExitCode cmd args ""
             case ecode of
                 ExitSuccess -> return ()
-                ExitFailure c -> print $ "Render of " ++ T.unpack output ++ "failed with code " ++ show c
+                ExitFailure c -> print $ "Render of " ++ output ++ "failed with code " ++ show c
 
 inkscape :: FilePath -> RenderJob -> IO ()
 inkscape input job = undefined
 
-render :: Vector RenderGroup -> IO ()
-render = V.mapM_ render'
-    where
-        render' (RenderGroup Illustrator inputs jobs) =
-            V.forM_ inputs $ \input ->
-                V.forM_ jobs $ \job ->
-                    illustrator input job
-        render' (RenderGroup Inkscape inputs jobs) = undefined
+render :: RenderGroup -> IO ()
+render (RenderGroup b inputs jobs) =
+    V.forM_ inputs $ \input ->
+        V.forM_ jobs $ \job -> do
+            createDirectoryIfMissing True $ jobPath job
+            let backFun = case b of
+                            Illustrator -> illustrator
+                            Inkscape -> inkscape
+            backFun input job
 
 main :: IO ()
 main = do
-    renderGroups <- decodeFileEither "/home/matt/workspace/rn/images.yaml"
+    args <- getArgs
+    let defaultPath = "/home/matt/workspace/rn/images.yaml"
+        yamlFile = fromMaybe defaultPath $ listToMaybe args
+    renderGroups <- decodeFileEither yamlFile
     case renderGroups of
       Left e -> print e
-      Right r -> render r
+      Right r -> V.mapM_ render r
